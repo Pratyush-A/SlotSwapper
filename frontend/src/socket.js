21ed496a7c0d31f5d2ad4f15043dce6ea8f7d77d
Pratyush-A <pratyushacharya34@gmail.com>
2025-11-05 16:59:22 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,15 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
-  transports: ["websocket"],
+const URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000" 
+    : "https://slotswapper-97z9.onrender.com"; 
+
+export const socket = io(URL, {
+  transports: ["websocket"], 
   reconnection: true,
   reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
+  withCredentials: true,
 });
+
