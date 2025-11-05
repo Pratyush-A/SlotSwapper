7c2b136c44f67a2ac7ca5b3e0fd38f5065bd8b40
Pratyush-A <pratyushacharya34@gmail.com>
2025-11-05 16:49:36 +0530

    Update server.js
    With live frontend link to get real time changes

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+     origin: [
+      "https://slot-swapper-murex.vercel.app", 
+      "http://localhost:5173" 
+    ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
   },
 });
